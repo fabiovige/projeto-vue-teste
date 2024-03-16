602cd3e1efaaa1f3afe323574cb8b6999f34d0bf
Fabio Martins <fabiovige@gmail.com>
2024-03-16 07:55:28 -0300

    adicionando projeto de teste

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="user">
+      <h1>{{ user.name }}</h1>
+      <p>{{ user.bio }}</p>
+      <img :src="user.avatar_url" alt="User avatar" />
+    </div>
+    <div v-else>
+      <p>Usuário não encontrado.</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'UserProfile',
+    data() {
+      return {
+        user: null
+      };
+    },
+    methods: {
+      fetchUserData() {
+        axios.get('https://api.github.com/users/fabiovige')
+          .then(response => {
+            this.user = response.data;
+          })
+          .catch(error => {
+            console.error("Erro ao buscar dados do usuário:", error);
+          });
+      }
+    },
+    mounted() {
+      this.fetchUserData();
+    }
+  };
+  </script>
+  
